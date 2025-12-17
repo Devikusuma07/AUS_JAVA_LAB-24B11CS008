efc151f8794581ab9f9ae79bbdc1b095cb3d7038
Devikusuma07 <devikusu714@gmail.com>
2025-12-17 14:34:25 +0530

    Add files via upload

---
+++ si.java
@@ -0,0 +1,10 @@
+public class si {
+    public static void main(String[] args) {
+        int p = 100000, t=24;
+        float r = 2.7f;
+        float sint = (p*r*t) / 100;
+        System.out.println("simple interst is: " + sint);
+        
+    }
+    
+}
