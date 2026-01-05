5d00dd44f084d98c475dcf43ebc5daa9e9217aaa
Devikusuma07 <devikusu714@gmail.com>
2026-01-05 14:43:15 +0530

    Add files via upload

---
+++ DatasTypes.java
@@ -0,0 +1,25 @@
+
+public class DatasTypes {
+    public static void main(String[] args) {
+
+        int n = 9;
+        byte by = 127;
+        short sh = 558;
+        long l = 5454L;
+
+        float f = 8.9f;
+        double d = 99.99;
+
+        char c = 'k';
+        boolean b = true;
+
+        System.out.println("int: " + n);
+        System.out.println("byte: " + by);
+        System.out.println("short: " + sh);
+        System.out.println("long: " + l);
+        System.out.println("float: " + f);
+        System.out.println("double: " + d);
+        System.out.println("char: " + c);
+        System.out.println("boolean: " + b);
+    }
+}
