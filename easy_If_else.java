eadc990bbdaefe52e787c04ebc9099773446db82
Devikusuma07 <devikusu714@gmail.com>
2026-01-18 16:03:37 +0530

    Add files via upload

---
+++ easy_If_else.java
@@ -0,0 +1,14 @@
+public class easy_If_else {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 8;
+
+        if(x > y) {
+            System.out.println(x);
+        } else {
+            System.err.println(y);
+        }
+        
+    }
+    
+}
