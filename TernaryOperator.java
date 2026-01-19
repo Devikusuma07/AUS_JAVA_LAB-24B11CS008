21fece36cc6e4ee54df8cc7af01a961ef885c442
Devikusuma07 <devikusu714@gmail.com>
2026-01-19 11:50:04 +0530

    Add files via upload

---
+++ TernaryOperator.java
@@ -0,0 +1,11 @@
+public class TernaryOperator {
+    public static void main(String[] args) {
+        int x = 7;
+        
+       int  result = x % 2 == 0 ? 10 : 20;
+
+        System.err.println("Result: " + result);
+
+    }
+    
+}
