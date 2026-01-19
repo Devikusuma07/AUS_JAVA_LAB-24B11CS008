66c0b7636cc8cc432fcf12625c1edd1417f6ea6b
Devikusuma07 <devikusu714@gmail.com>
2026-01-19 11:50:22 +0530

    Add files via upload

---
+++ SwitchCase.java
@@ -0,0 +1,33 @@
+public class SwitchCase {
+    public static void main(String[] args) {
+        int n = 3;
+
+        switch(n){
+            case 1:
+                System.err.println("Monday");
+                break;
+            case 2:
+                System.err.println("Tuesday");
+                break;
+            case 3:
+                System.err.println("Wednesday");
+                break;
+            case 4:
+                System.err.println("Thursday");
+                break;
+            case 5:
+                System.err.println("Friday");
+                break;
+            case 6:
+                System.err.println("Saturday");
+                break;
+            case 7:
+                System.err.println("Sunday");
+                break;
+            default:
+                System.err.println("Invalid number");
+        }
+        
+    }
+    
+}
