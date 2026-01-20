06fce1c1862e36dcfcc0e0698fa8ed077b3dd03c
Devikusuma07 <devikusu714@gmail.com>
2026-01-20 21:36:56 +0530

    Add files via upload

---
+++ whileLoop.java
@@ -0,0 +1,12 @@
+public class whileLoop {
+    public static void main(String[] args) {
+        int i = 1;
+
+        while(i <= 4) {
+            System.err.println("Hi" + i);
+            i++;
+        }
+
+    }
+    
+}
